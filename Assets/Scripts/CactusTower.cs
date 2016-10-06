49c62955f1cca977114c8f8934a25d25b5cf169e
Scott Troutman <troutmansm@gmail.com>
2016-10-05 23:18:45 -0500

    2 new towers
    cactus tower (maybe rename)
    -shoots 8 shots in a circle around it
    saloon tower
    -buffs nearby towers, does not shoot

---
+++ Assets/Scripts/CactusTower.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CactusTower : Tower
+{
+    public CactusTower() : base()
+    {
+        cost = 300;
+        range = 20;
+        damage = 50;
+        speed = 0.5;
+    }
+
+    protected override void Attack(GameObject target)
+    {
+        //run attack code
+        if (timeSinceLastShot <= 0)
+        {
+            Vector3[] offsets = { new Vector3(1,1,0), new Vector3(-1,1,0), new Vector3(0,1,1), new Vector3(0,1,-1),
+                                  new Vector3(Mathf.Sqrt(2),1,-Mathf.Sqrt(2)), new Vector3(-Mathf.Sqrt(2),1,-Mathf.Sqrt(2)), new Vector3(Mathf.Sqrt(2),1,Mathf.Sqrt(2)), new Vector3(-Mathf.Sqrt(2),1,Mathf.Sqrt(2))};
+            GameObject[] bullet = new GameObject[8];
+            for (int i = 0; i < 8; i++)
+            {
+                bullet[i] = (GameObject)Instantiate(bulletPrefab, transform.position + 1 * offsets[i], Quaternion.Euler(0, 90*i, 0));
+                bullet[i].GetComponent<Rigidbody>().velocity = bullet[i].transform.forward * 200.0f;
+                bullet[i].GetComponent<Bullet>().SetDamage(damage + damageBuff);
+                Destroy(bullet[i], 1);
+            }
+            timeSinceLastShot = 60 / (speed + speedBuff);
+        }
+    }
+}
---
+++ Assets/Scripts/SaloonTower.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaloonTower : Tower
+{
+    public SaloonTower() : base()
+    {
+        cost = 500;
+        range = 30;
+        damage = 50;
+        speed = 2;
+    }
+
+    protected override void ExtraActions()
+    {
+        GameObject[] towers = GameObject.FindGameObjectsWithTag("Tower");
+        for (int i = 0; i < towers.Length; i++)
+        {
+            //see if friendly tower is in range
+            if (Vector3.Distance(towers[i].transform.position, transform.position) < range)
+            {
+                towers[i].GetComponent<Tower>().GiveBuff(0.2, 0.3, 2);
+                Debug.DrawLine(transform.position, towers[i].transform.position);
+            }
+        }
+    }
+
+    protected override void Attack(GameObject target)
+    {
+        //saloon has no attack code        
+    }
+}
